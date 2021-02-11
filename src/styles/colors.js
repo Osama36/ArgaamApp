18ab3c96d9a07e6044e148d13317422bce1b32a8
Osama Zulfiqar Ali <osamazulfiqar36@gmail.com>
2021-02-11 13:21:29 +0500

    Color fix for dark mode

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
 
-let mode = "LIGHT";
+let mode = "LIGHsdT";
 
 export default COLORS = (mode == "LIGHT") ? {
   PRIMARY_BG: '#fff',
