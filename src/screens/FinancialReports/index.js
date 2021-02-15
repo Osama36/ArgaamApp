491c82d35f852c634be41b2bd72029a5837423be
Osama Ishaq <osama.ishaq10@gmail.com>
2021-02-15 17:04:10 +0500

    TextSizeChange

--- src/screens/FinancialReports/index.js
+++ src/screens/FinancialReports/index.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import { SafeAreaView, Text, View, TouchableOpacity, Image, KeyboardAvoidingView } from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  KeyboardAvoidingView,
+} from 'react-native';
 import styles from './style';
-import COLORS from '../../styles/colors'
-import { icons } from '../../utils/assets'
-import AppInput from '../../components/AppInput'
-import Button from '../../components/Button'
-
-console.log('lol');
+import COLORS from '../../styles/colors';
+import {icons} from '../../utils/assets';
 
 export default function index(props) {
   return (
-
     <View style={styles.container}>
       <View style={styles.SecondaryContBg} />
-      <View style={styles.InnerCont}>
-        {/* JSX HERE */}
-
-      </View>
+      <View style={styles.InnerCont}>{/* JSX HERE */}</View>
     </View>
-
-  )
+  );
 }
-
-
-
